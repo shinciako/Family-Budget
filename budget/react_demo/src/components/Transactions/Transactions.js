4ba4bec6e5012768ad4fd4f47c3c1cadf3c3fde5
shinciako <100320142+shinciako@users.noreply.github.com>
2022-12-14 20:49:53 +0100

    Added sorting by date in descending order

--- budget/react_demo/src/components/Transactions/Transactions.js
+++ budget/react_demo/src/components/Transactions/Transactions.js
@@ -8,9 +8,24 @@ import "./Transactions.css";
 const Transactions = (props) => {
   const [dateStart, setDateStart] = useState("");
   const [dateEnd, setDateEnd] = useState("");
-  const [filteredCategory, setFilteredCategory] = useState({id: 0, name: "All"});
+  const [filteredCategory, setFilteredCategory] = useState({
+    id: 0,
+    name: "All",
+  });
 
-  const options = [{id: 0, name: "All"},...props.categories]
+  const options = [{ id: 0, name: "All" }, ...props.categories];
+
+  const sortedTransactions = props.items.sort((a, b) => {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+    if (dateA < dateB) {
+      return 1;
+    } else if (dateA > dateB) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
 
   const dateStartChangeHandler = (selectedStart) => {
     setDateStart(selectedStart);
@@ -24,17 +39,15 @@ const Transactions = (props) => {
     setFilteredCategory(selectedCategory);
   };
 
-  const dateFilter = (date) =>{
-    if(dateStart==="" && dateEnd==="") return date
-    if(dateEnd==="") return (date >=new Date(dateStart));
-    if(dateStart==="") return (date <=new Date(dateEnd));
-    return (date >=new Date(dateStart) && date<=new Date(dateEnd));
+  const dateFilter = (date) => {
+    if (dateStart === "" && dateEnd === "") return date;
+    if (dateEnd === "") return date >= new Date(dateStart);
+    if (dateStart === "") return date <= new Date(dateEnd);
+    return date >= new Date(dateStart) && date <= new Date(dateEnd);
+  };
 
-  }
-
-  const filteredTransactions = props.items.filter((transaction) => {
-    if (filteredCategory.name === "All")
-      return(dateFilter(transaction.date));
+  const filteredTransactions = sortedTransactions.filter((transaction) => {
+    if (filteredCategory.name === "All") return dateFilter(transaction.date);
     return (
       dateFilter(transaction.date) &&
       transaction.category === filteredCategory?.name
