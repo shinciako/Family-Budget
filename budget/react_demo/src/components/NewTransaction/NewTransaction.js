5435821158f68dd98e306730b51f96ab72e62199
shinciako <100320142+shinciako@users.noreply.github.com>
2022-12-14 20:35:03 +0100

    Polish, added category form

--- budget/react_demo/src/components/NewTransaction/NewTransaction.js
+++ budget/react_demo/src/components/NewTransaction/NewTransaction.js
@@ -1,10 +1,12 @@
-import {useState} from 'react';
+import { useState } from "react";
 
 import TransactionForm from "./TransactionForm";
+import CategoryForm from "./CategoryForm";
 import "./NewTransaction.css";
 
 const NewTransaction = (props) => {
   const [isEditing, setIsEditing] = useState(false);
+  const [isCategoryEditing, setIsCategoryEditing] = useState(false);
 
   const saveTransactionDataHandler = (enteredTransactionData) => {
     props.onAddTransaction(enteredTransactionData);
@@ -12,18 +14,49 @@ const NewTransaction = (props) => {
     setIsEditing(false);
   };
 
-  const startEditingHandler = () =>{
-    setIsEditing(true);
-  }
+  const saveCategoryDataHandler = (enteredCategoryData) => {
+    props.onAddCategory(enteredCategoryData);
+    console.log(enteredCategoryData);
+    setIsCategoryEditing(false);
+  };
 
-  const stopEditingHandler = () =>{
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
     setIsEditing(false);
-  }
+  };
+
+  const startCategoryEditingHandler = () => {
+    setIsCategoryEditing(true);
+  };
+
+  const stopCategoryEditingHandler = () => {
+    setIsCategoryEditing(false);
+  };
 
   return (
     <div className="new-transaction">
-      {!isEditing && <button onClick={startEditingHandler}>Add new transaction</button>}
-      {isEditing && <TransactionForm onSaveTransactionData={saveTransactionDataHandler} onCancel={stopEditingHandler} categories={props.categories}/>}
+      {!isEditing && !isCategoryEditing && (
+        <button onClick={startEditingHandler}>Add new transaction</button>
+      )}
+      {!isEditing && !isCategoryEditing && (
+        <button onClick={startCategoryEditingHandler}>Add new Category</button>
+      )}
+      {isEditing && (
+        <TransactionForm
+          onSaveTransactionData={saveTransactionDataHandler}
+          onCancel={stopEditingHandler}
+          categories={props.categories}
+        />
+      )}
+      {isCategoryEditing && (
+        <CategoryForm
+          onSaveCategoryData={saveCategoryDataHandler}
+          onCancel={stopCategoryEditingHandler}
+        />
+      )}
     </div>
   );
 };
