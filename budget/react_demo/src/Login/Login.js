3bb720fa99761f633f8c5ae78ba38835896b1240
shinciako <100320142+shinciako@users.noreply.github.com>
2023-01-07 02:00:24 +0100

    Receving token on front

--- budget/react_demo/src/Login/Login.js
+++ budget/react_demo/src/Login/Login.js
@@ -1,20 +1,64 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import classes from "./Register.module.css";
 
 const Login = () => {
+  const [enteredEmail, setEnteredEmail] = useState();
+  const [enteredPassword, setEnteredPassword] = useState();
+
+  async function login(loginCredentials) {
+    await fetch("http://localhost:8080/login", {
+      method: "POST",
+      body: JSON.stringify(loginCredentials),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((response) => {
+      if (response.ok) {
+        response.json().then((json) => {
+          console.log(json);
+        });
+        window.location = '/';
+      }
+    });
+  }
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const loginData = {
+      email: enteredEmail,
+      password: enteredPassword,
+    };
+    login(loginData);
+  };
+
   return (
     <div className={classes.body}>
       <div className={classes.main}>
-        <Link to={"/register"} className={classes.sign}>Sign up</Link>
+        <Link to={"/register"} className={classes.sign}>
+          Sign up
+        </Link>
         <div className={classes.signup}>
-          <form>
-            <label className={classes.title}>
-              Login
-            </label>
-            <input type="email" name="email" placeholder="Email" required="" />
+          <form onSubmit={submitHandler}>
+            <label className={classes.title}>Login</label>
+            <input
+              type="email"
+              value={enteredEmail}
+              onChange={emailChangeHandler}
+              placeholder="Email"
+              required=""
+            />
             <input
               type="password"
-              name="pswd"
+              value={enteredPassword}
+              onChange={passwordChangeHandler}
               placeholder="Password"
               required=""
             />
--- budget/react_demo/src/Login/Register.js
+++ budget/react_demo/src/Login/Register.js
@@ -2,24 +2,28 @@ import { Link } from "react-router-dom";
 import classes from "./Register.module.css";
 
 const Register = () => {
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className={classes.body}>
       <div className={classes.main}>
       <Link to={"/login"} className={classes.sign}>Login</Link>
         <div className={classes.signup}>
-          <form>
+          <form onSubmit={submitHandler}>
             <label className={classes.title}>
               Sign up
             </label>
-            <input type="text" name="txt" placeholder="User name" required="" />
-            <input type="email" name="email" placeholder="Email" required="" />
+            <input type="text" placeholder="User name" required="" />
+            <input type="email" placeholder="Email" required="" />
             <input
               type="password"
-              name="pswd"
               placeholder="Password"
               required=""
             />
-            <button>Sign up</button>
+            <button type="submit">Sign up</button>
           </form>
         </div>
       </div>
